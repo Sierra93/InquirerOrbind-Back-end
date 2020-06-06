b60fbb5c447a0ee899d3055720afcffa7e1e4875
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-06-06 18:51:06 +0300

    Fix adding answer.

---
+++ InquirerOrbind-Back-end.Core/BuyPrize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InquirerOrbind_Back_end.Core {
+    /// <summary>
+    /// Модель описывает покупку подарка или бонуса.
+    /// </summary>
+    public class BuyPrize {
+        public int Id { get; set; }
+
+        public int PrizeId { get; set; }    // Id подарка или бонуса, на который пользователь меняет баллы.
+    }
+}
--- InquirerOrbind-Back-end/Controllers/PrizeController.cs
+++ InquirerOrbind-Back-end/Controllers/PrizeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InquirerOrbind_Back_end.Core;
 using InquirerOrbind_Back_end.Data;
 using InquirerOrbind_Back_end.Models;
 using Microsoft.AspNetCore.Http;
@@ -36,8 +37,16 @@ namespace InquirerOrbind_Back_end.Controllers {
         /// </summary>
         /// <returns></returns>
         [HttpPost, Route("buy")]
-        public async Task<IActionResult> Buy([FromBody] UserSignIn user) {
-            //var oUser = await db
+        public async Task<IActionResult> Buy([FromBody] BuyPrize user) {
+            // Находит пользователя.
+            var oUser = await db.UserDetails.Where(u => u.Id == user.Id).FirstOrDefaultAsync();
+            int userPoints = oUser.Points;
+
+            // Выбирает подарок или бонус, которым интересуется пользователь.
+            var oPrize = await db.Prizes.Where(p => p.Id == user.PrizeId).FirstOrDefaultAsync();
+
+            // Проверяет, хватает ли бонусов для обмена.
+            //if ()
 
             return Ok();    // Вернуть купленный приз. через {}
         }
